fbf623814516660d98dc1e84383b189c202927a4
CorvaeOboro <joethiel@gmail.com>
2021-10-21 13:54:03 -0700

    Chivalry Spells icons colorized

---
+++ UI_SpellsChivalry/00_create_montage_spells.bat
@@ -0,0 +1,18 @@
+montage ^
+ui_chivalry_spell_1_0x5103.bmp ^
+ui_chivalry_spell_1_0x5108.bmp ^
+ui_chivalry_spell_1_0x5100.bmp ^
+ui_chivalry_spell_1_0x5102.bmp ^
+ui_chivalry_spell_1_0x5101.bmp ^
+ui_chivalry_spell_1_0x5105.bmp ^
+ui_chivalry_spell_1_0x5107.bmp ^
+ui_chivalry_spell_1_0x5104.bmp ^
+ui_chivalry_spell_1_0x5109.bmp ^
+ui_chivalry_spell_1_0x5106.bmp ^
+-density 72 ^
+-tile 5x2 ^
+-geometry 88x88+8+8 ^
+-border 8 ^
+-bordercolor black ^
+-background black ^
+ui_spell_chivalry_comp.png
---
+++ UI_SpellsChivalry/00_num_to_name.bat
@@ -0,0 +1,12 @@
+ren 11012.bmp ui_chivalry_book_0x2B04.bmp
+ren 20736.bmp ui_chivalry_spell_1_0x5100.bmp
+ren 20737.bmp ui_chivalry_spell_1_0x5101.bmp
+ren 20738.bmp ui_chivalry_spell_1_0x5102.bmp
+ren 20739.bmp ui_chivalry_spell_1_0x5103.bmp
+ren 20740.bmp ui_chivalry_spell_1_0x5104.bmp
+ren 20741.bmp ui_chivalry_spell_1_0x5105.bmp
+ren 20742.bmp ui_chivalry_spell_1_0x5106.bmp
+ren 20743.bmp ui_chivalry_spell_1_0x5107.bmp
+ren 20744.bmp ui_chivalry_spell_1_0x5108.bmp
+ren 20745.bmp ui_chivalry_spell_1_0x5109.bmp
+ren 20996.bmp ui_chivalry_spell_b_0x5204.bmp
