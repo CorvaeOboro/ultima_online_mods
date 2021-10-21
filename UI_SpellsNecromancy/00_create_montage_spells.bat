aefde567cb4a331a1acd28a76efc81ccff3bd3be
CorvaeOboro <joethiel@gmail.com>
2021-10-21 13:53:03 -0700

    Necromancy Spells icons colorized versions

---
+++ UI_SpellsNecromancy/00_create_montage_spells.bat
@@ -0,0 +1,18 @@
+montage ^
+ui_necro_spell_1_0x500D.bmp ^
+ui_necro_spell_1_0x5009.bmp ^
+ui_necro_spell_1_0x5004.bmp ^
+ui_necro_spell_1_0x5002.bmp ^
+ui_necro_spell_1_0x500C.bmp ^
+ui_necro_spell_1_0x5007.bmp ^
+ui_necro_spell_1_0x5001.bmp ^
+ui_necro_spell_1_0x500A.bmp ^
+ui_necro_spell_1_0x500E.bmp ^
+ui_necro_spell_1_0x5008.bmp ^
+-density 72 ^
+-tile 5x2 ^
+-geometry 88x88+8+8 ^
+-border 8 ^
+-bordercolor black ^
+-background black ^
+ui_spell_necro_comp.png
