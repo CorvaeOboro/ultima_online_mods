9197c44dd4aadc83aab5dc5ce49cd50aeb82c9b9
DESKTOP-27J6S69\Corva <corvaeoboro@gmail.com>
2024-04-28 19:43:34 -0700

    Chivalry and Necro upscale reorg rename by spell name

---
+++ UI/UI_SpellsNecromancy/Upscale/00_create_montage_spells.bat
@@ -0,0 +1,19 @@
+montage ^
+ui_necro_spell_1_0x500D.bmp ^
+ui_necro_spell_1_0x5009.bmp ^
+ui_necro_spell_1_0x5004.bmp ^
+ui_necro_spell_1_0x5002.bmp ^
+ui_necro_spell_1_0x500C.bmp ^
+ui_necro_spell_1_0x5007.bmp ^
+ui_necro_spell_1_0x5001.bmp ^
+ui_necro_spell_1_0x500A.bmp ^
+ui_necro_spell_1_0x500E.bmp ^
+ui_necro_spell_1_0x5008.bmp ^
+-density 72 ^
+-tile 5x2 ^
+-geometry 88x88+4+4 ^
+-border 4 ^
+-bordercolor black ^
+-background black ^
+-interpolate Nearest -filter point ^
+ui_spell_necro_comp.png
